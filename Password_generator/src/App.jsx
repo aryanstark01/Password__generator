0198eaf05cb951f1adff97939d15400e46966f62
Aryan Raj <aryanstark1234@gmail.com>
2024-11-21 22:01:14 +0530

    second

--- Password_generator/src/App.jsx
+++ Password_generator/src/App.jsx
@@ -4,7 +4,7 @@ import { useCallback, useEffect, useRef, useState } from 'react'
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [length, setLength] = useState(8);
+  const [length, setLength] = useState(8 );
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("")
